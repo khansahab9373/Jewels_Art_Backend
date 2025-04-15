0bd5f71f07d90c4e245853832635d61a8211b1b7
khansahab9373 <abdulrahmankhan1011@gmail.com>
2025-04-16 00:58:19 +0530

    adding cors public

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors()); // Uncomment if needed
+app.use(
+  cors({
+    origin: "*", // Allow all
+  })
+);
+// Uncomment if needed
 
 // Database connection
 connectDB(process.env.DBURL);
