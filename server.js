d2b71881cc2239cc2b8c2b0897204b50d369deb5
khansahab9373 <abdulrahmankhan1011@gmail.com>
2025-04-16 14:29:14 +0530

    changes

--- server.js
+++ server.js
@@ -23,5 +23,6 @@ connectDB(process.env.DBURL);
 // Routes
 app.use("/", userRoutes); // Use user routes
 
-// Export app for Vercel
-export default app;
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`); // Log server start message
+});
